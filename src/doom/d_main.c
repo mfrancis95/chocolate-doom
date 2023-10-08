e66885340307c352558920459b6bbd965b20696e
Michael Francis <Michael.Francis@ibm.com>
2023-10-07 21:08:14 -0400

    Introduce 1.5 compatibility

--- src/doom/d_main.c
+++ src/doom/d_main.c
@@ -982,6 +982,7 @@ static struct
     GameVersion_t version;
 } gameversions[] = {
     {"Doom 1.2",             "1.2",        exe_doom_1_2},
+    {"Doom 1.5",             "1.5",        exe_doom_1_5},
     {"Doom 1.666",           "1.666",      exe_doom_1_666},
     {"Doom 1.7/1.7a",        "1.7",        exe_doom_1_7},
     {"Doom 1.8",             "1.8",        exe_doom_1_8},
